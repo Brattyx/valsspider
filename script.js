1746e737bcb982cc5220213dc005f5a2f721f331
Brattyx <92929215+Brattyx@users.noreply.github.com>
2022-12-11 16:36:19 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+$('.toggle-button').on('click', function() {
+  $('.left-sidebar').toggleClass('minimize'); 
+});
+
+$('.user-profile').on('click', function() {
+  $('.left-sidebar').toggleClass('minimize'); 
+});
